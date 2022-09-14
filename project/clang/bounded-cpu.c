2160d89f78feb5759ac2dcd5bb71b77476ee491e
burgess01 <burgess01@allegheny.edu>
2022-09-14 15:07:26 -0400

    Add bounded c code

--- project/clang/bounded-cpu.c
+++ project/clang/bounded-cpu.c
@@ -1,12 +1,29 @@
-// TODO: Add required imports
+// Add required imports
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  // TODO: check for three arguments:
+int main(int argc, char *argv[])
+{
+  // check for three arguments:
   // 1: program name
   // 2: program input as a string
   // 3: number of iterations for the loop
-  // TODO: extract the three command-line arguments
-  // TODO: iterate for a number of times driven
-  // by the looping maximum bound given by user
-  // TODO: make sure that the program output matches the README file
+  if (argc == 3)
+  {
+    // extract the three command-line arguments
+    char program_name[] = argv[0];
+    char user_input[] = argv[1];
+    int iterations = argv[2];
+
+    // iterate for a number of times driven
+    // by the looping maximum bound given by user
+    for (int i = 1; i < iterations; i++)
+    {
+      // make sure that the program output matches the README file
+      println("Run %s with %s \n", program_name, user_input);
+    }
+  }
+  else
+  {
+    println("Please enter three command line arguments, one being the file name.");
+  }
 }
