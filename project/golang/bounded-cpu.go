c7142e01a11a32e8eb377af1850bb4b9b6005c9f
burgess01 <burgess01@allegheny.edu>
2022-09-14 14:51:06 -0400

    Add Go bounded program

--- project/golang/bounded-cpu.go
+++ project/golang/bounded-cpu.go
@@ -1,10 +1,20 @@
 package main
 
-// TODO: Add all of the required imports
+// import the required packages
+import "fmt"
+import "os"
+import "time"
 
-func main() {
-	// TODO: extract the command-line arguments
-	// TODO: convert the command-line argument(s) as needed
-	// TODO: iterate through the required runs of the program
-	// TODO: make sure that the program output matches the README file
+func main(value, num) {
+	// extract the command-line arguments
+	var prog_name = os.Args[0]
+	var entered_value = os.Args[1]
+	var num_of_runs = os.Args[2]
+	// convert the command-line argument(s) as needed
+	// iterate through the required runs of the program
+	for i := 1; i < num_of_runs; i++ {
+		// make sure that the program output matches the README file
+		time.Sleep(1*time.Second)
+		fmt.Println("Run ", i, " of", prog_name, " with ", entered_value)
+	}
 }
