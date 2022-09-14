813a72fa2975e9920d8dcdd647353a350a8a9a76
burgess01 <burgess01@allegheny.edu>
2022-09-14 14:51:13 -0400

    Add go unbounded example

--- project/golang/unbounded-cpu.go
+++ project/golang/unbounded-cpu.go
@@ -1,11 +1,22 @@
 package main
 
 // import the required packages
+import "fmt"
+import "os"
+import "time"
 
 func main() {
-	// TODO: declare a done variable that is a boolean
-	// TODO: indicate that the program is not done running
-	// TODO: extract the command-line argument(s)
-	// TODO: always iterate in an unbounded-fashion
-	// TODO: make sure that the program output matches the README file
+	// declare a done variable that is a boolean
+	var done bool
+	// indicate that the program is not done running
+	done = false
+	// extract the command-line argument(s)
+	var prog_name = os.Args[0]
+	var entered_value = os.Args[1]
+	// always iterate in an unbounded-fashion
+	for !done{
+		// TODO: make sure that the program output matches the README file
+		time.Sleep(1*time.Second)
+		fmt.Println("Run", prog_name, " ", entered_value)
+	}
 }
