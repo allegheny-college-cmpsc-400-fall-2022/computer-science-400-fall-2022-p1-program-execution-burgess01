40998cb0d978f10d39ef1ab17293c4c9dcd034c3
burgess01 <burgess01@allegheny.edu>
2022-09-14 15:34:21 -0400

    Finished bounded Go program

--- project/golang/bounded-cpu.go
+++ project/golang/bounded-cpu.go
@@ -4,17 +4,21 @@ package main
 import "fmt"
 import "os"
 import "time"
+import "strconv"
 
-func main(value, num) {
+func main() {
 	// extract the command-line arguments
 	var prog_name = os.Args[0]
 	var entered_value = os.Args[1]
-	var num_of_runs = os.Args[2]
+	var number = os.Args[2]
 	// convert the command-line argument(s) as needed
+	num_of_runs, err := strconv.Atoi(number)
 	// iterate through the required runs of the program
-	for i := 1; i < num_of_runs; i++ {
-		// make sure that the program output matches the README file
-		time.Sleep(1*time.Second)
-		fmt.Println("Run ", i, " of", prog_name, " with ", entered_value)
+	if err == nil{
+		for i := 1; i < (num_of_runs + 1); i++{
+			// make sure that the program output matches the README file
+			time.Sleep(1*time.Second)
+			fmt.Println("Run ", i, " of", prog_name, " with ", entered_value)
+		}
 	}
 }
