7fa93c6a942292ced23fdd205697838e242d1b9e
burgess01 <burgess01@allegheny.edu>
2022-09-14 15:56:46 -0400

    Finish bounded C program

--- project/clang/bounded-cpu.c
+++ project/clang/bounded-cpu.c
@@ -1,5 +1,6 @@
 // Add required imports
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -10,20 +11,17 @@ int main(int argc, char *argv[])
   if (argc == 3)
   {
     // extract the three command-line arguments
-    char program_name[] = argv[0];
-    char user_input[] = argv[1];
-    int iterations = argv[2];
-
     // iterate for a number of times driven
     // by the looping maximum bound given by user
-    for (int i = 1; i < iterations; i++)
+    int num_of_iterations = atoi(argv[2]);
+    for (int i = 1; i < (num_of_iterations + 1); i++)
     {
       // make sure that the program output matches the README file
-      println("Run %s with %s \n", program_name, user_input);
+      printf("Run %d of %s with %s \n", i, argv[0], argv[1]);
     }
   }
   else
   {
-    println("Please enter three command line arguments, one being the file name.");
+    printf("Please enter three command line arguments, one being the file name.");
   }
 }
